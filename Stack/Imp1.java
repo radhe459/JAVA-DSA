3fbdf02e94d8fa0e1585aab3a62113ba6abd2cd7
Radhe Shyam Chaubey <informativersc1234@gmail.com>
2023-01-24 22:50:50 +0530

    Stack added

---
+++ Stack/Imp1.java
@@ -0,0 +1,73 @@
+package Stack;
+
+import java.util.*;
+
+class Stackimp1{
+    int max;
+    int top;
+    int a[];
+    public Stackimp1(int size){
+        top=-1;
+        max=size;
+        a=new int[size];
+    }
+    
+    public void push(int ele){
+        if(isfull()){
+            System.out.println("stack is full");
+        }
+        else{
+            a[++top]=ele;
+            System.out.println("pushed element is :"+ele);
+        }
+    }
+    public int pop(){
+        if(isempty()){
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        }
+       
+        return a[top--];
+        
+    }
+    public int peek(){
+        if(isempty()){
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        }
+        return a[top];
+    }
+    public int size(){
+        return top+1;
+    }
+    public boolean isfull(){
+        if(top>=max-1)
+            return true;
+        return false;
+    }
+    
+    public boolean isempty(){
+        if(top==-1)
+            return true;
+        return false;
+    }
+}
+public class Imp1 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the size of stack : ");
+        int n=s.nextInt();
+        Stackimp1 ob=new Stackimp1(n);
+        ob.push(5);
+        ob.push(10);
+        ob.push(12);
+        ob.push(13);
+        System.out.println("Element on the top of Stack: "+ob.peek());
+        System.out.println("Popped Element: "+ob.pop());
+        System.out.println("Element on the top of Stack: "+ob.peek());
+        System.out.println("Size of stack : "+ob.size());
+        System.out.println("Empty :"+ob.isempty());
+        System.out.println("Full :"+ob.isfull());
+        
+    }
+}
---
+++ Stack/imp2.java
@@ -0,0 +1,48 @@
+
+package Stack;
+
+import java.util.*;
+
+class Stackimp2{
+    ArrayList<Integer> list=new ArrayList<Integer>();
+    public void push(int n){
+        list.add(n);
+        System.out.println("Pushed : "+list.get(list.size()-1));
+    }
+    
+    public int pop(){
+        if(empty()){
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        }
+        
+        return list.remove(list.size()-1);
+    }
+    public int peek(){
+        if(empty()){
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        }
+        return list.get(list.size()-1);
+    }
+    public int size(){
+        return list.size();
+    }
+    public boolean empty(){
+        return list.isEmpty();
+    }
+}
+
+public class imp2 {
+    public static void main(String[] args) {
+        Stackimp2 ob=new Stackimp2();
+        ob.push(5);
+        ob.push(10);
+        ob.push(55);
+        ob.push(66);
+        System.out.println("Popped : "+ob.pop());
+        System.out.println("Popped : "+ob.peek());
+        System.out.println("Size : "+ob.size());
+        System.out.println("Empty : "+ob.empty());
+    }
+}
